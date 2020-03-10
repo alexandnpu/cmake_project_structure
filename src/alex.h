bc760c688b66145035d6c627120bf19cddaedd15
alexandnpu <alexandnpu@outlook.com>
2020-03-10 20:09:22 +0800

    initial commit

---
+++ src/alex.h
@@ -0,0 +1,15 @@
+#ifndef ALEXLIBRARY_ALEX_H
+#define ALEXLIBRARY_ALEX_H
+
+#ifdef __cplusplus
+extern "C"{
+#endif 
+
+int add(int a, int b);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //ALEXLIBRARY_ALEX_H
---
+++ test/unit/Add-test.cpp
@@ -0,0 +1,6 @@
+#include "gtest/gtest.h"
+#include "alex.h"
+
+TEST(alexTest, test1) {
+    EXPECT_EQ (add(1, 2),  3);
+}
